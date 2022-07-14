1ced55932ba50f44cb043bbc0f095906a1f1977b
Vikram Kumar <104130605+Vikramkumarcoder@users.noreply.github.com>
2022-07-14 12:34:06 +0530

    Add files via upload

---
+++ Basics/Menus and submenus.py
@@ -0,0 +1,41 @@
+#pylint:disable=W0105
+#pylint:disable=W0401
+from tkinter import *
+
+root = Tk()
+root. title("Menu")
+root. geometry ("649x563")
+def func():
+    Label(text="hello"). pack()
+
+"""use it for non-dropdown menu"""
+
+#mymenu = Menu(root)
+#mymenu. add_command(label = "Menu", command = func)
+#mymenu. add_command(label = "Exit", command = quit)
+#root.config(menu = mymenu)
+
+"""Use it for drop-down menu"""
+
+#File Menus and their submenus
+mainfile = Menu(root)
+m1 = Menu(mainfile, tearoff = 1)
+m1.add_command(label = "New Project", command = func)
+m1.add_command(label = "save", command = func)
+m1.add_separator()#for separation line between two submenus
+m1.add_command(label = "save as", command = func)
+m1.add_command(label = "Print", command = func)
+root.config(menu = mainfile)
+mainfile.add_cascade(label = "File", menu = m1)
+
+
+# Edit menu and their submenus
+m2 = Menu(mainfile, tearoff = 0)
+m2.add_command(label = "Copy", command = func)
+m2.add_command(label = "Paste", command = func)
+m2.add_command(label = "Insert", command = func)
+m2.add_command(label = "Cut", command = func)
+mainfile.add_cascade(label = "Edit", menu = m2)
+
+
+root. mainloop()
